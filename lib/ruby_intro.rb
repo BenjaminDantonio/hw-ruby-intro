658ba69f240650478d16325b75a215a5f33bb54a
unknown <benjamin.dantonio@gmail.com>
2020-01-25 15:50:21 -0600

    Complete hw assignment.

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,81 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  if arr.length() == 0
+	return 0
+  end
+  arr.reduce(:+)
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.length() == 0
+	return 0
+  end
+  arr.max(2).reduce(:+)
+  
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  if arr.combination(2).find{|x,y| x+y==n} != nil
+	return true
+  end
+  false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, ".concat(name)
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s.scan(/\A[a-z]/i) != [] && s.scan(/\A[^aeiou]/i) != []
+    return true
+  end
+  false
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  #s = s.scan(/\S/).join(', ')
+  if s.length() == 0 || s.scan(/\D/) != [] || s.to_i(2) % 4 != 0
+    return false
+  end
+  true
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+
+  def initialize(isbn, price)
+    if isbn.length() == 0
+	  raise ArgumentError, 'ISBN must not be empty'
+	end
+	if price <= 0
+	  raise ArgumentError, 'Price must not be less than or equal to 0'
+	end
+	@isbn = isbn
+	@price = price
+  end
+  
+  def isbn
+	@isbn
+  end
+  
+  def isbn=(isbn)
+    @isbn = isbn
+  end
+  
+  def price
+	@price
+  end
+  
+  def price=(price)
+    @price = price
+  end
+  
+  def price_as_string
+    price = @price
+	sprintf("$%0.2f", price)
+  end
 end
